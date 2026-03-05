4d4b04eeab49632df1bbb9f0ef93197360f5c737
mieszko <najgorzej111@gmail.com>
2026-03-05 23:34:08 +0200

    chore: fixing 403 error, method POST

--- website/script.js
+++ website/script.js
@@ -6,7 +6,7 @@ window.onload = function () {
 }
 
 async function getData() {
-    const response = await fetch("https://616wetav3j.execute-api.eu-central-1.amazonaws.com/prod/counter-path")
+    const response = await fetch("https://616wetav3j.execute-api.eu-central-1.amazonaws.com/prod/counter-path", {method: "POST"})
     if (!response.ok) {
         console.log("error")
         return
