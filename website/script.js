73ec460a5af527821ce94970d375ed0a80c8b391
mieszko <najgorzej111@gmail.com>
2026-03-06 00:02:33 +0200

    chore: debug

--- website/script.js
+++ website/script.js
@@ -1,11 +1,13 @@
 window.onload = function () {
     if (localStorage.getItem("hasCodeRunBefore") === null) {
+        console.log("starting")
         getData()
         localStorage.setItem("hasCodeRunBefore", true);
     }
 }
 
 async function getData() {
+    console.log("fetching")
     const response = await fetch("https://616wetav3j.execute-api.eu-central-1.amazonaws.com/prod/counter-path", {method: "POST"})
     if (!response.ok) {
         console.log("error")
