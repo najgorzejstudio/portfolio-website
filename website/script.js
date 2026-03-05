1f60e0f84ebccf7bdbe88a278f1db331d65573fe
mieszko <najgorzej111@gmail.com>
2026-03-05 23:30:34 +0200

    chore: corrected api url

--- website/script.js
+++ website/script.js
@@ -6,17 +6,18 @@ window.onload = function () {
 }
 
 async function getData() {
-  const response = await fetch("https://616wetav3j.execute-api.eu-central-1.amazonaws.com/prod")
-  const data = await response.json()
-  
-}
+    const response = await fetch("https://616wetav3j.execute-api.eu-central-1.amazonaws.com/prod/counter-path")
+    if (!response.ok) {
+        console.log("error")
+        return
+    }
 
-var data = JSON.parse(this.response)
+    const data = await response.json()
+    
 
-if (request.status >= 200 && request.status < 400) {
     console.log(data)
-} else {
-   console.log('error')
+
+
+    document.getElementById("counter").innerHTML = "This website was viewed: " + data["visits"] + "times"
 }
 
-document.getElementById("counter").innerHTML = "This website was viewed: " + data["counter"] + "times"
